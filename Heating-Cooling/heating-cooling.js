659055796fd87d9b709fc3be57b759d4f112ffb9
James Vail <jvail777@yahoo.com>
2022-10-13 18:47:49 -0400

    New Labs Repo

---
+++ Heating-Cooling/heating-cooling.js
@@ -0,0 +1,13 @@
+let actualTemp = 68;
+let desiredTemp = 68;
+//Extended Challange
+let tempCelsius = 62;
+let targetUnit = "F";
+
+if (actualTemp < desiredTemp) {
+  console.log("Run heat");
+} else if (actualTemp > desiredTemp) {
+  console.log("Run A/C");
+} else if (actualTemp == desiredTemp) {
+  console.log("Standby");
+}
---
+++ JS-Refresher-Lab/Refresher.js
@@ -0,0 +1,16 @@
+let firstName = "James";
+let lastName = "Vail";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+let population = 672351;
+console.log(population);
+let math = population * 3;
+console.log(math);
+let javaScriptIsCool = true;
+let colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+for (let i = 1; i < 6; i++) {
+  console.log(i);
+}
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
